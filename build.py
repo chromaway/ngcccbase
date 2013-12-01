1286bdcf179818603a18fb950bf1875cdd660316
Daniel Roberts <Ademan555@gmail.com>
2013-12-01 00:21:42 -0800

    Added required files.

--- build.py
+++ build.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 
 import sys
+
+import os
+import os.path
+
 from cx_Freeze import setup, Executable
-#from setuptools import find_packages
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-build_exe_options = {}
+def ls(path):
+    for name in os.listdir(path):
+        yield os.path.join(path, name)
+
+import itertools
+
+build_exe_options = {
+    "include_files": list(itertools.chain(ls('ui/forms'), ls('ui/icons')))
+}
 
 setup(
     name='ngcccbase',
@@ -20,7 +31,7 @@ setup(
     url='https://github.com/bitcoinx/ngcccbase',
     keywords='bitcoinx bitcoin coloredcoins',
     packages=["ngcccbase", "ngcccbase.services", "ngcccbase.p2ptrade", "ecdsa", "coloredcoinlib", "ui"],
-    #options = {"build_exe": build_exe_options},
+    options = {"build_exe": build_exe_options},
     executables = [Executable("ngccc-server.py", base=base),
         Executable("ngccc-gui.py", base=base),
         Executable("ngccc-cli.py", base=base),
