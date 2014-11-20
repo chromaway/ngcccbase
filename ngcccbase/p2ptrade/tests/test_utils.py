67b565c28a2446c27b8fc1828b585c651cf91801
F483 <fabian.barkhau@gmail.com>
2014-11-20 18:39:15 +0100

    added test_utils

---
+++ ngcccbase/p2ptrade/tests/test_utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import unittest
+
+
+class TestMakeRandomId(unittest.TestCase):
+
+  def test_is_random(self):
+    pass # TODO test it
+
+  def test_8_bytes_of_entropy(self):
+    pass # TODO test it
+
+  def test_is_hex_str(self):
+    pass # TODO test it
+
+
+class TestHTTPInterface(unittest.TestCase):
+
+  def test_poll(self):
+    pass # TODO test it
+
+  def test_post(self):
+    pass # TODO test it
+
+
+if __name__ == '__main__':
+    unittest.main()
+
