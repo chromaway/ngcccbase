332fd1bf4fdbc7f14c575a73eddc09cf2706eb8e
Jorgen Modin <jorgen@webworks.se>
2015-05-22 19:45:02 +0200

    Testing json-rpc server with realnet and testnet config files

--- ngcccbase/tests/test_json_api_server.py
+++ ngcccbase/tests/test_json_api_server.py
@@ -5,23 +5,54 @@ import os
 import signal
 import subprocess
 import time
+import json
 import pyjsonrpc
 
-class TestJSONAPIServer(unittest.TestCase):
+class TestJSONAPIServer(unittest.TestCase):        
 
     def setUp(self):
         self.client = pyjsonrpc.HttpClient(url = "http://localhost:8080")
-        self.server = subprocess.Popen('python ngccc-server.py startserver', preexec_fn=os.setsid, shell=True)
-        time.sleep(4) # make it settle before calling stuff
-
-    def test_dumpconfig(self):
-        dumped_config = self.client.dumpconfig()
-        self.assertTrue(dumped_config.has_key('testnet') )
 
     def tearDown(self):
-        os.killpg(os.getpgid(self.server.pid), signal.SIGTERM)
+        pass
 
-    # def test_load_server_config(self):
+    def test_load_config_realnet(self):
+        config = {
+                "testnet": False,
+                "port": 8080,
+                "hostname": "localhost",
+                "wallet_path": "/tmp/realnet.wallet"
+                  }
+
+        with open('/tmp/config.json', 'w') as fi:
+            json.dump(config, fi)
+
+        server = subprocess.Popen('python ngccc-server.py startserver --config_path=/tmp/config.json', preexec_fn=os.setsid, shell=True)
+        time.sleep(4)
+        dumped_config = self.client.dumpconfig()
+        self.assertTrue(dumped_config.has_key('testnet') )
+        self.assertFalse(dumped_config['testnet'])
+        # killing server, and its child process (that would be HTTPServer)
+        os.killpg(os.getpgid(server.pid), signal.SIGTERM)
+
+    def test_load_config_testnet(self):
+        config = {
+                "testnet": True,
+                "port": 8080,
+                "hostname": "localhost",
+                "wallet_path": "/tmp/testnet.wallet"
+                  }
+
+        server = subprocess.Popen('python ngccc-server.py startserver --config_path=/tmp/config.json', preexec_fn=os.setsid, shell=True)
+        time.sleep(4)
+        with open('/tmp/config.json', 'w') as fi:
+            json.dump(config, fi)
+        dumped_config = self.client.dumpconfig()
+        self.assertTrue(dumped_config.has_key('testnet') )
+        self.assertFalse(dumped_config['testnet'])
+
+        # killing server, and its child process (that would be HTTPServer)
+        os.killpg(os.getpgid(server.pid), signal.SIGTERM)
 #
 if __name__ == '__main__':
     unittest.main()
