bdc845e416026a02b46cfc9836473d17ce9e302a
Jorgen Modin <jorgen@webworks.se>
2015-06-04 16:59:13 +0200

    Test for importprivkeys, refactoring

--- ngcccbase/tests/test_json_api_server.py
+++ ngcccbase/tests/test_json_api_server.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import unittest
 import traceback
 import os
@@ -13,15 +12,11 @@ import pyjsonrpc
 from ngcccbase import sanitize
 
 
-
-
-
 class TestJSONAPIServer(unittest.TestCase):   
 
     def setUp(self):
         self.reset_status()
         self.client = pyjsonrpc.HttpClient(url = "http://localhost:8080")
-
 
     def reset_status(self):
         self.server = None
@@ -34,25 +29,21 @@ class TestJSONAPIServer(unittest.TestCase):
             shutil.rmtree(self.working_dir)
         self.reset_status()
 
-    def create_livenet_server(self):
-        """Start server with custom config on realnet"""
+    def create_server(self, testnet=False):
         self.working_dir = tempfile.mkdtemp()
         config_path = self.working_dir + '/config.json'
 
         config = {
-                "testnet": False,
+                "testnet": testnet,
                 "port": 8080,
                 "hostname": "localhost",
-                "wallet_path": self.working_dir + "/realnet.wallet"
+                "wallet_path": self.working_dir + "/coloredcoins.wallet"
                   }
         with open(config_path, 'w') as fi:
             json.dump(config, fi)
 
         self.server = subprocess.Popen('python ngccc-server.py startserver --config_path=%s' % config_path, preexec_fn=os.setsid, shell=True)
-        time.sleep(4)
-
-
-
+        time.sleep(4)        
 
     def test_default_config(self):
         """See to that server starts and pulls in a config.json file"""
@@ -62,34 +53,25 @@ class TestJSONAPIServer(unittest.TestCase):
 
     def test_load_config_realnet(self):
         """Start server with custom config on realnet"""
-        self.create_livenet_server()
+        self.create_server()
         self.assertFalse(self.client.dumpconfig()['testnet'])
 
     def test_load_config_testnet(self):
         """Start server with custom config on testnet"""
-        config = {
-                "testnet": True,
-                "port": 8080,
-                "hostname": "localhost",
-                "wallet_path": "/tmp/testnet.wallet"
-                  }
-        with open('/tmp/config.json', 'w') as fi:
-            json.dump(config, fi)
-
-        self.server = subprocess.Popen('python ngccc-server.py startserver --config_path=/tmp/config.json', preexec_fn=os.setsid, shell=True)
-        time.sleep(4)
+        self.create_server(testnet=True)
         self.assertTrue(self.client.dumpconfig()['testnet'])
 
     def test_get_new_bitcoin_address(self):
-        """Start server with custom config on realnet"""
-        self.create_livenet_server()
+        """Generate a new bitcoin address"""
+        self.create_server()
         res = self.client.newaddress('bitcoin')
         self.assertEqual(len(res), 34) # may want a better test, with e.g. python-bitcoinaddress package
         self. assertTrue(sanitize.bitcoin_address(res))
 
     def test_scan_does_not_throw_exception(self):
-        """Start server with custom config on realnet"""
-        self.create_livenet_server()
+        """Make sure that scan and fullrescan return JSON and not 
+           something that causes an exception in pyjsonrpc"""
+        self.create_server()
         try:
             res = self.client.scan()
         except:
@@ -101,15 +83,24 @@ class TestJSONAPIServer(unittest.TestCase):
             self.fail('Fullrescan raised an exception\n' + traceback.format_exc())
             pass
 
-
     def test_importprivkey(self):
-        """Start server with custom config on realnet"""
+        """Test of importing private keys"""
         private_key = '5JTuHqTdknhZSnk5pBZaqWDaSuhz6xmJEc9fH9UXgvpZbdRNsLq'
         bitcoin_address = '1Jtkin4FsUcPW3VwcpNsYLmP82wC1ybv1Z'
-        self.create_livenet_server()
+        self.create_server()
         res = self.client.importprivkey('bitcoin', private_key)
         addresses = self.client.listaddresses('bitcoin')
         self.assertTrue(bitcoin_address in addresses)
+
+    def test_importprivkeys(self):
+        """Test of importing private keys"""
+        private_keys = ['5J7i6VrN6kedk2EfB3eLnJ79bxw5MJyAas8BwKwoziMPyKDrgi4','5KYXdVsaYs7m9oVXmLeRzCPfVTgbYoZEzCW3uQkMBya3vkcaPWT']
+        bitcoin_addresses = ['14nsAgexJULTvmsA82455ArmoNRgnxToGj','1PpgeDc29AzZJJ4sr4wn7bHSEeXVJo5B7n']
+        self.create_server()
+        res = self.client.importprivkeys('bitcoin', wifs = private_keys)
+        addresses = self.client.listaddresses('bitcoin')
+        for bitcoin_address in bitcoin_addresses:
+            self.assertTrue(bitcoin_address in addresses)
 
 if __name__ == '__main__':
     unittest.main()
