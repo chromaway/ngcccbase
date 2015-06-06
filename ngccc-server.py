609ed43e02ca83a9d4d8bfafa111942127437521
Jorgen Modin <jorgen@webworks.se>
2015-06-06 18:29:43 +0200

    Using argparse for more robust argument parsing

--- ngccc-server.py
+++ ngccc-server.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
-
-
 import sys
 import json
+import argparse
 from ngcccbase.api import Ngccc
 
+parser = argparse.ArgumentParser(description='Chromawallet JSON-RPC server')
+parser.add_argument('--config_path', default='config.json')
+parser.add_argument('start_server')
+
+args = parser.parse_args()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "Usage: %s <config_path>" % sys.argv[0]
-    else:
-        with open(sys.argv[1], 'r') as fo:
-            config = json.load(fo)
-            api = Ngccc(wallet=config["wallet"], testnet=config["testnet"])
-            api.startserver(hostname=config["hostname"], port=config["port"])
+    with open(args.config_path, 'r') as fo:
+        config = json.load(fo)
+        api = Ngccc(wallet=config["wallet_path"], testnet=config["testnet"])
+        api.startserver(hostname=config["hostname"], port=config["port"])
