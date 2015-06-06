4588113c3d592ebe8ccc877f60a04e94a8331612
Jorgen Modin <jorgen@webworks.se>
2015-06-06 18:28:36 +0200

    Making tests work according to documentation

--- ngcccbase/tests/test_json_api_server.py
+++ ngcccbase/tests/test_json_api_server.py
@@ -9,107 +9,100 @@ import shutil
 import tempfile
 import json
 import pyjsonrpc
-from ngcccbase import sanitize
+from pycoin.key.validate import is_address_valid
 
 
-def start_server(testnet, working_dir):
-    config = {
-        "testnet": testnet,
-        "port": "8080",
-        "hostname": "localhost",
-        "wallet": working_dir + "/coloredcoins.wallet"
-    }
-    config_path = working_dir + '/config.json'
-    with open(config_path, 'w') as fi:      
-        json.dump(config, fi)
-
-    cmd = 'python ngccc-server.py ' + config_path
-    return subprocess.Popen(cmd, preexec_fn=os.setsid, shell=True)
-
-
-class TestMainnet(unittest.TestCase):   
+class TestJSONAPIServer(unittest.TestCase):
 
     def setUp(self):
-        # setup server
-        self.working_dir = tempfile.mkdtemp()
-        self.server = start_server(False, self.working_dir)
-        time.sleep(4)        
+        self.reset_status()
+        self.client = pyjsonrpc.HttpClient(url="http://localhost:8080")
 
-        # setup client
-        self.client = pyjsonrpc.HttpClient(url = "http://localhost:8080")
-    
+    def reset_status(self):
+        self.server = None
+        self.working_dir = None
+
     def tearDown(self):
         if self.server:
             os.killpg(os.getpgid(self.server.pid), signal.SIGTERM)
         if self.working_dir:
             shutil.rmtree(self.working_dir)
+        self.reset_status()
 
-    def test_config_mainnet(self):
+    def create_server(self, testnet=False):
+        self.working_dir = tempfile.mkdtemp()
+        config_path = self.working_dir + '/config.json'
+
+        config = {
+            "testnet": testnet,
+            "port": 8080,
+            "hostname": "localhost",
+            "wallet_path": self.working_dir + "/coloredcoins.wallet"
+        }
+        with open(config_path, 'w') as fi:
+            json.dump(config, fi)
+
+        self.server = subprocess.Popen('python ngccc-server.py startserver --config_path=%s'
+                                       % config_path, preexec_fn=os.setsid, shell=True)
+        time.sleep(4)
+
+    def test_default_config(self):
+        """See to that server starts and pulls in a config.json file"""
+        self.server = subprocess.Popen('python ngccc-server.py startserver',
+                                       preexec_fn=os.setsid, shell=True)
+        time.sleep(4)
+        self.assertTrue(self.client.dumpconfig().has_key('testnet'))
+
+    def test_load_config_realnet(self):
         """Start server with custom config on realnet"""
+        self.create_server()
         self.assertFalse(self.client.dumpconfig()['testnet'])
+
+    def test_load_config_testnet(self):
+        """Start server with custom config on testnet"""
+        self.create_server(testnet=True)
+        self.assertTrue(self.client.dumpconfig()['testnet'])
 
     def test_get_new_bitcoin_address(self):
         """Generate a new bitcoin address"""
-        res = self.client.newaddress('bitcoin')
-        self.assertEqual(len(res), 34) # may want a better test, with e.g. python-bitcoinaddress package
-        self. assertTrue(sanitize.bitcoin_address(res))
+        self.create_server()
+        address = self.client.newaddress('bitcoin')
+        netcodes = ['BTC']
+        self.assertTrue(bool(is_address_valid(address, allowable_netcodes=netcodes)))
 
     def test_scan_does_not_throw_exception(self):
-        """Make sure that scan and fullrescan return JSON and not 
+        """Make sure that scan and fullrescan return JSON and not
            something that causes an exception in pyjsonrpc"""
+        self.create_server()
         try:
             res = self.client.scan()
         except:
             self.fail('Scan raised an exception\n' + traceback.format_exc())
-            pass
         try:
             res = self.client.fullrescan()
         except:
             self.fail('Fullrescan raised an exception\n' + traceback.format_exc())
-            pass
 
     def test_importprivkey(self):
         """Test of importing private keys"""
         private_key = '5JTuHqTdknhZSnk5pBZaqWDaSuhz6xmJEc9fH9UXgvpZbdRNsLq'
         bitcoin_address = '1Jtkin4FsUcPW3VwcpNsYLmP82wC1ybv1Z'
+        self.create_server()
         res = self.client.importprivkey('bitcoin', private_key)
         addresses = self.client.listaddresses('bitcoin')
         self.assertTrue(bitcoin_address in addresses)
 
     def test_importprivkeys(self):
         """Test of importing private keys"""
-        private_keys = ['5J7i6VrN6kedk2EfB3eLnJ79bxw5MJyAas8BwKwoziMPyKDrgi4','5KYXdVsaYs7m9oVXmLeRzCPfVTgbYoZEzCW3uQkMBya3vkcaPWT']
-        bitcoin_addresses = ['14nsAgexJULTvmsA82455ArmoNRgnxToGj','1PpgeDc29AzZJJ4sr4wn7bHSEeXVJo5B7n']
-        
-        wifs = json.dumps(private_keys)
-        res = self.client.importprivkeys('bitcoin', wifs=wifs)
+        private_keys = ['5J7i6VrN6kedk2EfB3eLnJ79bxw5MJyAas8BwKwoziMPyKDrgi4',
+                        '5KYXdVsaYs7m9oVXmLeRzCPfVTgbYoZEzCW3uQkMBya3vkcaPWT']
+        bitcoin_addresses = ['14nsAgexJULTvmsA82455ArmoNRgnxToGj',
+                             '1PpgeDc29AzZJJ4sr4wn7bHSEeXVJo5B7n']
+        self.create_server()
+        res = self.client.importprivkeys('bitcoin', wifs=private_keys)
         addresses = self.client.listaddresses('bitcoin')
         for bitcoin_address in bitcoin_addresses:
             self.assertTrue(bitcoin_address in addresses)
 
-
-class TestTestnet(unittest.TestCase):   
-
-    def setUp(self):
-        # setup server
-        self.working_dir = tempfile.mkdtemp()
-        self.server = start_server(True, self.working_dir)
-        time.sleep(4)        
-
-        # setup client
-        self.client = pyjsonrpc.HttpClient(url = "http://localhost:8080")
-    
-    def tearDown(self):
-        if self.server:
-            os.killpg(os.getpgid(self.server.pid), signal.SIGTERM)
-        if self.working_dir:
-            shutil.rmtree(self.working_dir)
-
-    def test_config_testnet(self):
-        """Start server with custom config on testnet"""
-        self.assertTrue(self.client.dumpconfig()['testnet'])
-
-
 if __name__ == '__main__':
     unittest.main()
-
