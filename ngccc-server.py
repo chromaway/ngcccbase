1597e1bfe66ddeac602aff9be816e3bacbd16658
Jorgen Modin <jorgen@webworks.se>
2015-07-23 21:10:44 +0200

    Moving parser stuff so that in only kicks in when called from the command line

--- ngccc-server.py
+++ ngccc-server.py
@@ -5,12 +5,13 @@ import argparse
 from ngcccbase.api import Ngccc
 from ngcccbase import testing_config
 
-parser = argparse.ArgumentParser(description='Chromawallet JSON-RPC server')
-parser.add_argument('--config_path', default='config.json')
 
-args = parser.parse_args()
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Chromawallet JSON-RPC server')
+    parser.add_argument('--config_path', default='config.json')
+
+    args = parser.parse_args()
     with open(args.config_path, 'r') as fo:
         config = json.load(fo)
         testing_config.regtest_server = config.get('regtest_server', None)
