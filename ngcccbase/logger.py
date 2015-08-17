6738ff5a847eb03704cbd5e0ebcf062322c4fb19
Jorgen Modin <jorgen@webworks.se>
2015-08-18 01:25:44 +0200

    Adding logging to rotating log file

--- ngcccbase/logger.py
+++ ngcccbase/logger.py
@@ -1,6 +1,18 @@
 import logging
+import logging.handlers
+import os
+
 
 def setup_logging():
     logger = logging.getLogger('ngcccbase')
+    formatter = logging.Formatter('%(module)s:%(funcName)s:%(lineno)d: - "%(message)s" [%(threadName)s - LogLevel%(levelno)s - %(levelname)s ] %(asctime)s')
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.StreamHandler())
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    log_file = os.path.join(dir_path, 'logs', 'ngcccbase.log')
+    fh = logging.handlers.RotatingFileHandler(
+              log_file, maxBytes=2000000, backupCount=1)
+    sh = logging.StreamHandler()
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    # logger.addHandler(sh)
+# 
