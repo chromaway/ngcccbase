1dc5face81412504c7628e701f58811504dfd681
Jorgen Modin <jorgen@webworks.se>
2015-08-20 17:24:22 +0200

    Commenting out logger due to segfaults
    Even the ConcurrentRotatingFileHandler seems to contribute to the
    segfaults.

--- ngcccbase/logger.py
+++ ngcccbase/logger.py
@@ -1,15 +1,16 @@
 import logging
 import logging.handlers
 import os
-
+import cloghandler
 
 def setup_logging():
     logger = logging.getLogger('ngcccbase')
-    formatter = logging.Formatter('%(module)s:%(funcName)s:%(lineno)d: - "%(message)s" [%(threadName)s - LogLevel%(levelno)s - %(levelname)s ] %(asctime)s')
-    logger.setLevel(logging.DEBUG)
-    dir_path = os.path.dirname(os.path.abspath(__file__))
-    log_file = os.path.join(dir_path, 'logs', 'ngcccbase.log')
-    fh = logging.handlers.RotatingFileHandler(
-              log_file, maxBytes=2000000, backupCount=1)
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
+    # formatter = logging.Formatter(
+    #     '%(module)s:%(funcName)s:%(lineno)d: - "%(message)s" [%(threadName)s - LogLevel%(levelno)s - %(levelname)s ] %(asctime)s')
+    # logger.setLevel(logging.DEBUG)
+    # dir_path = os.path.dirname(os.path.abspath(__file__))
+    # log_file = os.path.join(dir_path, 'logs', 'ngcccbase.log')
+    # fh = cloghandler.ConcurrentRotatingFileHandler(
+    #           log_file, maxBytes=2000000, backupCount=1)
+    # fh.setFormatter(formatter)
+    # logger.addHandler(fh)
