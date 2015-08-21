cf9b17c451d5468222bbb1ce9de6f9a1b28c0024
Jorgen Modin <jorgen@webworks.se>
2015-08-21 17:16:46 +0200

    Using restapihandler with proper formatting

--- ngcccbase/logger.py
+++ ngcccbase/logger.py
@@ -1,16 +1,29 @@
 import logging
 import logging.handlers
 import os
-import cloghandler
+from restapi_logging_handler import RestApiHandler as BaseRestApiHandler
+
+class RestApiHandler(BaseRestApiHandler):
+    def _getPayload(self, record):
+        """
+        The data that will be sent to the RESTful API
+        """
+        payload = {
+            'log': record.name,
+            'level': logging.getLevelName(record.levelno),
+            'message': self.format(record)
+        }
+        tb = self._getTraceback(record)
+        if tb:
+            payload['traceback'] = tb
+        return payload
 
 def setup_logging():
     logger = logging.getLogger('ngcccbase')
-    # formatter = logging.Formatter(
-    #     '%(module)s:%(funcName)s:%(lineno)d: - "%(message)s" [%(threadName)s - LogLevel%(levelno)s - %(levelname)s ] %(asctime)s')
-    # logger.setLevel(logging.DEBUG)
-    # dir_path = os.path.dirname(os.path.abspath(__file__))
-    # log_file = os.path.join(dir_path, 'logs', 'ngcccbase.log')
-    # fh = cloghandler.ConcurrentRotatingFileHandler(
-    #           log_file, maxBytes=2000000, backupCount=1)
-    # fh.setFormatter(formatter)
-    # logger.addHandler(fh)
+    formatter = logging.Formatter(
+        '%(module)s:%(funcName)s:%(lineno)d: - "%(message)s" [%(threadName)s - LogLevel%(levelno)s - %(levelname)s ] %(asctime)s')
+    logger.setLevel(logging.DEBUG)
+    fh = RestApiHandler('http://localhost:20520/endpoint')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
