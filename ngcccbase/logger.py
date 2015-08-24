e60c66916cec1d874f382dcc2ea1cea1bbd531fd
Jorgen Modin <jorgen@webworks.se>
2015-08-24 16:41:03 +0200

    More advanced logging,only used if RestApiHandler is available

--- ngcccbase/logger.py
+++ ngcccbase/logger.py
@@ -1,7 +1,13 @@
 import logging
 import logging.handlers
 import os
-from restapi_logging_handler import RestApiHandler as BaseRestApiHandler
+develop_mode = True
+try:
+    from restapi_logging_handler import RestApiHandler as BaseRestApiHandler
+except ImportError:
+    develop_mode = False
+    BaseRestApiHandler = object
+
 
 class RestApiHandler(BaseRestApiHandler):
     def _getPayload(self, record):
@@ -23,7 +29,10 @@ def setup_logging():
     formatter = logging.Formatter(
         '%(module)s:%(funcName)s:%(lineno)d: - "%(message)s" [%(threadName)s - LogLevel%(levelno)s - %(levelname)s ] %(asctime)s')
     logger.setLevel(logging.DEBUG)
-    fh = RestApiHandler('http://localhost:20520/endpoint')
+    if develop_mode:
+        fh = RestApiHandler('http://localhost:20520/endpoint')
+    else:
+        fh = StreamHandler()
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
