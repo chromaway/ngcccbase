1da9e4876873f83e5de2db41fa72656defeeb22f
Jorgen Modin <jorgen@webworks.se>
2015-08-25 15:50:44 +0200

    Switching to multiprocessing_logging

--- ngcccbase/logger.py
+++ ngcccbase/logger.py
@@ -16,58 +16,22 @@
 import logging
 import logging.handlers
 import os
-try:
-    from restapi_logging_handler import RestApiHandler as BaseRestApiHandler
-    log_over_http = True
-except ImportError:
-    log_over_http = False
-    BaseRestApiHandler = object
-
-DEBUG_LEVEL = os.environ.get('NGCCC_DEBUG_LEVEL', False)
-DEBUG_SERVER_PORT = os.environ.get('NGCCC_DEBUG_SERVER_PORT', 20520)
-LOG_TO_TERMINAL = os.environ.get('NGCCC_LOG_TO_TERMINAL', False)
-
-level_by_name = {}
-for level in (10, 20, 30, 40, 50):
-    level_by_name[logging.getLevelName(level)] = level
 
 
-class RestApiHandler(BaseRestApiHandler):
+import multiprocessing_logging
 
-    def setName(self, name='RestApiHandler'):
-        self.name = name
-
-    def _getPayload(self, record):
-
-        payload = {
-            'log': record.name,
-            'level': logging.getLevelName(record.levelno),
-            'message': self.format(record)  # Apply the formatter to records before sending them.
-        }
-        tb = self._getTraceback(record)
-        if tb:
-            payload['traceback'] = tb
-        return payload
-
+log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs', 'ngcccbase.log')
+mhandler = multiprocessing_logging.MultiProcessingHandler('worker-logger', sub_handler= logging.handlers.RotatingFileHandler(
+              log_file, maxBytes=20000000, backupCount=0))
 
 def setup_logging():
-
     logger = logging.getLogger('ngcccbase')
-    if log_over_http and not LOG_TO_TERMINAL:
-        endpoint = 'http://localhost:%s/endpoint' % DEBUG_SERVER_PORT
-        fh = RestApiHandler(endpoint)
-        fh.setName("Logging with RestApiHandler to '%s' (pid %s)" % (endpoint, os.getpid()))
-    else:
-        fh = logging.StreamHandler()
-        fh.setName("Logging with StreamHandler (pid %s)" % (os.getpid(), ))
-    if DEBUG_LEVEL in (level_by_name.keys()):
-        logger.setLevel(level_by_name[DEBUG_LEVEL])
-        formatter = logging.Formatter(  # Verbose format for filtering with e.g. grep
+    logger.setLevel(logging.CRITICAL)
+    formatter = logging.Formatter(  # Verbose format for filtering with e.g. grep
             '%(module)s:%(funcName)s:%(lineno)d: - "%(message)s" [%(threadName)s pid%(process)d- LogLevel%(levelno)s - %(levelname)s ] %(asctime)s')
-        fh.setFormatter(formatter)
-    else:
-        logger.setLevel(logging.CRITICAL)
-
-    if not fh.name in [h.name for h in logger.handlers]:
-        logger.addHandler(fh)
-        print fh.name
+    mhandler.setFormatter(formatter)
+    mhandler.setLevel(logging.DEBUG)
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.CRITICAL)
+    logger.addHandler(mhandler)
+    logger.addHandler(sh)
