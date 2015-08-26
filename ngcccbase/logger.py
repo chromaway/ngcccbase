d376c8076ef02d0a18cf48a0ea824cf8254be51d
Jorgen Modin <jorgen@webworks.se>
2015-08-27 00:34:43 +0200

    Adding a Nullhandler to make logger not print DEBUG records to terminal
    In some integration tests, the logger starts outputting DEBUG records to
    the terminal after a while, while continuing to print to file. The
    problem may lie with a different log import or configuration in a
    specific module. In the meantime, this NullHandler prevents the
    behavior.

--- ngcccbase/logger.py
+++ ngcccbase/logger.py
@@ -4,19 +4,23 @@ import os
 
 
 import multiprocessing_logging
-
-log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs', 'ngcccbase.log')
-mhandler = multiprocessing_logging.MultiProcessingHandler('worker-logger', sub_handler= logging.handlers.RotatingFileHandler(
-              log_file, maxBytes=20000000, backupCount=0))
+root = logging.getLogger()
+root.addHandler(logging.NullHandler())
+root.setLevel(logging.DEBUG)
 
 def setup_logging():
-    logger = logging.getLogger('ngcccbase')
-    logger.setLevel(logging.CRITICAL)
+    log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs', 'ngcccbase.log')
+    mhandler = multiprocessing_logging.MultiProcessingHandler('worker-logger', sub_handler= logging.handlers.RotatingFileHandler(
+              log_file, maxBytes=20000000, backupCount=0))
     formatter = logging.Formatter(  # Verbose format for filtering with e.g. grep
             '%(module)s:%(funcName)s:%(lineno)d: - "%(message)s" [%(threadName)s pid%(process)d- LogLevel%(levelno)s - %(levelname)s ] %(asctime)s')
     mhandler.setFormatter(formatter)
     mhandler.setLevel(logging.DEBUG)
+    logger = logging.getLogger('ngcccbase')
+
     sh = logging.StreamHandler()
     sh.setLevel(logging.CRITICAL)
     logger.addHandler(mhandler)
     logger.addHandler(sh)
+    # logger.setLevel(logging.CRITICAL)
+
